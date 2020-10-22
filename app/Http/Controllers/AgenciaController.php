a9f88de153e041f487e8d136a6df0535069c6b6d
barruco <51138491+barruco@users.noreply.github.com>
2020-10-22 01:25:58 -0600

    Create AgenciaController.php
    En esta clase construimos nuestro controlador sobre toda la estructrura la tabla agencia.

---
+++ app/Http/Controllers/AgenciaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\agencia;
+
+use Illuminate\Http\Request;
+
+class AgenciaController extends Controller
+{
+    //
+    public function vista(){
+        return  view('agregar');
+    }
+
+    public function create(Request $request){
+        $agencia = new agencia();
+
+        $agencia -> nombre = $request -> nombre;
+        $agencia -> direccion = $request -> direccion;
+        $agencia -> telefono = $request -> telefono;
+        $agencia -> id_departamento = $request -> id_departamento;
+        $agencia -> id_municipio = $request -> id_municipio;
+        $agencia -> id_region = $request -> id_region;
+        $agencia -> id_tipo_agencia = $request -> id_tipo_agencia;
+
+        $agencia -> save();
+
+        return redirect('/create');
+
+    }
+}
