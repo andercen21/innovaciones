da4d941cbd8d7dce1c1ec5a2703e212aea62be55
barruco <51138491+barruco@users.noreply.github.com>
2020-10-22 01:29:15 -0600

    Update web.php
    Establecemos la ruta para poder acceder a la vista de agencia.

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/','AgenciaController@vista');
 
 Auth::routes();
 
