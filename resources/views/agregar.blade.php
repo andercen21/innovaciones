6ac5f688435d4d3885c71fb12290da7aa22602c4
barruco <51138491+barruco@users.noreply.github.com>
2020-10-22 21:08:19 -0600

    Update agregar.blade.php
    creacion de formulario en html para el enmaquetado del CRUD de Agencia

--- resources/views/agregar.blade.php
+++ resources/views/agregar.blade.php
@@ -2,9 +2,36 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
-    <title>agregar agencia</title>
-   
+    <title>agregar agencia</title> 
 </head>
 <body>
+    <center>
+    <form action="/create" method="POST">
+        {{ csrf_field() }}
+        <label>Nombre de Agencia</label>
+        <input type="text" name="nombre" placeholder="nombre de agencia">
+        <br>
+        <label>Direccion</label>
+        <input type="text" name="direccion" placeholder="direccion">
+        <br>
+        <label>Telefono</label>
+        <input type="text" name="telefono" placeholder="telefono">
+        <br>
+        <label>Departamento</label>
+        <input type="text" name="id_departamento" placeholder="departamento">
+        <br>
+        <label>Municipio</label>
+        <input type="text" name="id_municipio" placeholder="municipio">
+        <br>
+        <label>Region</label>
+        <input type="text" name="id_region" placeholder="region">
+        <br>
+        <label>Tipo de Agencia</label>
+        <input type="text" name="id_tipo_agencia" placeholder="tipo de agencia">
+        <br>
+        <br>
+        <button type="submit">Agregar Agencia</button>
+    </form>
+    </center>
 </body>
 </html>  
