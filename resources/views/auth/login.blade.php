30f002fd42c2b0d242ba2e2235ebaa624b633f69
andercen21 <73188295+andercen21@users.noreply.github.com>
2020-11-02 09:21:56 -0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
 
